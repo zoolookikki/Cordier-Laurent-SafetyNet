ab1a309510fea71b092dbebc82628101b5fb5932
Laurent Cordier <laurent.cordier28@orange.fr>
2024-12-17 11:20:00 +0100

    Spring Boot project initialized

---
+++ src/test/java/com/cordierlaurent/safetynet/SafetynetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cordierlaurent.safetynet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetynetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
