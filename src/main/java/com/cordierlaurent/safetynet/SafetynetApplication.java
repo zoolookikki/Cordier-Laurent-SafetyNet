70d5938fe5eed0201153f9b45f96164200ceba86
Laurent Cordier <laurent.cordier28@orange.fr>
2024-12-19 09:41:53 +0100

    Setting up log4j2

--- src/main/java/com/cordierlaurent/safetynet/SafetynetApplication.java
+++ src/main/java/com/cordierlaurent/safetynet/SafetynetApplication.java
@@ -2,12 +2,26 @@ package com.cordierlaurent.safetynet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @SpringBootApplication
-public class SafetynetApplication {
+public class SafetynetApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
+    private static final Logger logger = LogManager.getLogger(SafetynetApplication.class);
+
+    public static void main(String[] args) {
 		SpringApplication.run(SafetynetApplication.class, args);
+    }
+
+    // mémo : après démarrage complet de spring (même si Logger n'en a pas besoin).
+    @Override
+	public void run(String... args) throws Exception {
+	    logger.info("test display info");
+	    logger.debug("test display debug");
+	    logger.error("test display error");
 	}
 
 }
+
