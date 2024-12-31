ec634130b87a811ae1b90f5ab9546779ec307df5
Laurent Cordier <laurent.cordier28@orange.fr>
2024-12-31 10:46:49 +0100

    Forgot to change in the Json result file: personBasicInformationsDTO by persons

--- src/main/java/com/cordierlaurent/safetynet/dto/PersonsCoveredByFireStationDTO.java
+++ src/main/java/com/cordierlaurent/safetynet/dto/PersonsCoveredByFireStationDTO.java
@@ -2,12 +2,15 @@ package com.cordierlaurent.safetynet.dto;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class PersonsCoveredByFireStationDTO {
+    @JsonProperty("persons")  // Change le nom de la clé.
     private List<PersonBasicInformationsDTO> personBasicInformationsDTO;
     private int numberOfAdults;
     private int numberOfChildren;    
