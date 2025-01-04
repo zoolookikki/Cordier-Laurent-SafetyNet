1d03210521bdf2c50d27f5b0a7a2608618a0c248
Laurent Cordier <laurent.cordier28@orange.fr>
2025-01-04 19:21:25 +0100

    forgot to move /firestation?stationNumber=<station_number> in FireStationInfosController

--- src/main/java/com/cordierlaurent/safetynet/controller/FireStationInfosController.java
+++ src/main/java/com/cordierlaurent/safetynet/controller/FireStationInfosController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cordierlaurent.safetynet.dto.FireDTO;
+import com.cordierlaurent.safetynet.dto.PersonsCoveredByFireStationDTO;
 import com.cordierlaurent.safetynet.service.FireStationService;
 
 import lombok.extern.log4j.Log4j2;
@@ -19,6 +20,55 @@ public class FireStationInfosController {
     @Autowired
     private FireStationService fireStationService;
     
+    // implémentation de l'url qui demande la liste des personnes corresponant à une station : http://localhost:8080/firestation?stationNumber=<station_number>
+    /*
+    recherche firestations par station => liste d'adresses.
+    recherche personnes par adresse => liste de personnes.
+    dans cette liste de personnes il ne faut avoir que le prénom + nom + adresse + téléphone.
+    calculer le nombre de personnes > et <=18. 
+    Renvoyer un json sous la forme :
+    {
+        "persons": [
+        {
+          "firstName": "xxxx",
+          "lastName": "xxxx",
+          "address": "xxxx",
+          "phone": "xxxx"
+        },
+        {
+          "firstName": "yyyy",
+          "lastName": "yyyy",
+          "address": "yyyy",
+          "phone": "yyyy"
+        }
+      ],
+      "numberOfAdults": ?,
+      "numberOfChildren": ?
+    }
+    */
+    
+    // @GettMapping : mappe une requête HTTP GET à une méthode de contrôleur : lecture.
+    // @RequestParam : pour récupérer le paramètre passé en ? (ou & si plusieurs).
+    @GetMapping("/firestation")
+    public ResponseEntity<?> getPersonsCoveredByFireStation(@RequestParam("stationNumber") int stationNumber){
+        log.debug("appel de : /firestation?stationNumber=<station_number>");
+        
+        PersonsCoveredByFireStationDTO personsCoveredByFireStationDTO = fireStationService.findPersonsCoveredByFireStation(stationNumber);
+
+        // la liste de personnes est vide => rien trouvé.
+        if (personsCoveredByFireStationDTO.getPersonBasicInformationsDTO().isEmpty()) {
+            log.info("getPersonsCoveredByFireStation : " + this.getClass().getSimpleName() + " : non trouvé ");
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND) // 404 
+                    .build(); // là, il ne faut peut être rien dire dans ce cas de figure => build : réponse sans body.
+        }
+        // il y a au moins une personne => ok.
+        log.info("getPersonsCoveredByFireStation : " + this.getClass().getSimpleName() + " : " +  personsCoveredByFireStationDTO.getPersonBasicInformationsDTO().size() + " trouvé(s) ");
+        return ResponseEntity
+                .status(HttpStatus.OK) // 200
+                .body(personsCoveredByFireStationDTO); 
+    }
+
     // implémentation de l'url qui retourne les personnes habitants à une adresse donnée avec le numéro de la caserne de pompiers la déservant. 
     // Cette liste doit inclure également le numéro de téléphone, l'âge et les antécédents mécidaux de chaque personne.
     // http://localhost:8080/fire?address=<address>
